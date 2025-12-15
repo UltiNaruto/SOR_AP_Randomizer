4d9cb6949790eb4f4895fb8b57cd82f73111e84c
UltiNaruto <zenaruto@ymail.com>
2025-12-15 20:12:43 +0100

    Fixed dynamic logic import functions

--- src/Utils.py
+++ src/Utils.py
@@ -3,13 +3,14 @@ import importlib
 import inspect
 import os
 import re
+import zipfile
 from itertools import chain
 from pathlib import Path
 from types import ModuleType
 from typing import Iterable, Optional, TypeVar, Union
 
 MAGIC_EMPTY_SEED = ' ' * 20
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 
 STAGES: list[str] = [
     'Shopping Mall',
@@ -27,31 +28,73 @@ STAGES: list[str] = [
 T = TypeVar('T')
 
 
+def _extract_paths_from(path: str) -> tuple[str, Optional[str]]:
+    if '.apworld' in path:
+        tmp_index = path.find(f'.apworld/')
+        if tmp_index == -1:
+            tmp_index = path.find(f'.apworld\\')
+        if tmp_index == -1:
+            raise RuntimeError("Shouldn't happen! Couldn't get apworld separator index for apworld path and inner path!")
+        tmp_index += len('.apworld/')
+        apworld_path = path[:tmp_index-1]
+        return f'{path[tmp_index:].replace("\\", "/")}', apworld_path
+    else:
+        return path, None
+
+
 def _import_all_submodules(mod: Union[str, ModuleType], parent_submodule="") -> list[ModuleType]:
+    # Shouldn't happen but keep it for safety
+    if parent_submodule.endswith('.'):
+        return []
+
     if type(mod) is str:
-        path = Path(mod)
+        if Path(mod).suffix != '':
+            mod = os.path.dirname(mod)
+        path, apworld_path = _extract_paths_from(mod)
         if parent_submodule == "":
-            parent_submodule = f"{__package__}.{os.path.basename(mod)}"
+            parent_submodule = f"{__package__}.{os.path.basename(path)}"
     elif type(mod) is ModuleType:
-        path = Path(mod.__file__).parent
+        path, apworld_path = _extract_paths_from(os.path.dirname(mod.__file__))
         if parent_submodule == "":
-            parent_submodule = f"{__package__}.{os.path.basename(str(path))}"
+            parent_submodule = f"{__package__}.{os.path.basename(path)}"
     else:
         raise RuntimeError("Shouldn't happen, but import_all_submodules first argument is not str or ModuleType!")
 
-    submodules = glob.glob(f"{str(path)}/*")
-    submodules = [
-        *[os.path.basename(f)[:-3] for f in submodules if # exclude __init__.py
-         (os.path.isfile(f) and not os.path.basename(f).startswith('__') and Path(f).suffix == ".py")],
-        *[f for f in submodules
-          if os.path.isdir(f)],
-    ]
+    # we want either folders or python modules
+    if apworld_path is None:
+        submodules = glob.glob(f"{path}/*")
+        submodules = [
+            *[os.path.basename(f) for f in submodules if # exclude __init__.py
+             (os.path.isfile(f) and not os.path.basename(f).startswith('__') and Path(f).suffix == ".py")],
+            *[os.path.basename(f) for f in submodules
+              if os.path.isdir(f)],
+        ]
+    else:
+        submodules = []
+        with zipfile.ZipFile(apworld_path) as zip:
+            for f in zip.namelist():
+                # add files if they are in the current directory
+                # else only add directory of current directory for recursivity
+                if f.startswith(path):
+                    rel_path = f.replace(f'{path}\\'.replace('\\', '/'), '')
+                    # no need to parse an empty rel_path
+                    if rel_path == '':
+                        continue
+                    # for folders zipfile keeps a separator at the end so remove it
+                    if rel_path.endswith('/'):
+                        rel_path = rel_path[:-1]
+                    if not '/' in rel_path and not Path(f).name.startswith('__') and Path(f).suffix in ['.py', '']:
+                        submodules.append(rel_path)
+
     ret = []
     for submodule in submodules:
-        if os.path.isdir(submodule):
-            ret.extend(_import_all_submodules(submodule, f'{parent_submodule}.{os.path.basename(submodule)}'))
+        if not submodule.endswith(".py"):
+            submodule_path = str(Path(path).joinpath(submodule))
+            if apworld_path is not None:
+                submodule_path = str(Path(apworld_path).joinpath(submodule_path))
+            ret.extend(_import_all_submodules(submodule_path, f'{parent_submodule}.{os.path.basename(submodule)}'))
         else:
-            ret.append(importlib.import_module(f".{submodule}", parent_submodule))
+            ret.append(importlib.import_module(f".{submodule[:-3]}", parent_submodule))
     return ret
 
 
